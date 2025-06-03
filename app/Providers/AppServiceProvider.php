afc58e2e634ad7c81fdc2975be16405df097173b
uche-novajii <uche.l@novajii.com>
2025-06-03 23:15:38 +0100

    removed redundant binds

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,16 +12,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         
-         // Repository bindings
-         $this->app->bind(\App\Repositories\UserRepository::class, \App\Repositories\UserRepository::class);
-         $this->app->bind(\App\Repositories\ProjectRepository::class, \App\Repositories\ProjectRepository::class);
-         
-         // Service bindings
-         $this->app->bind(\App\Services\AuthService::class, \App\Services\AuthService::class);
-         $this->app->bind(\App\Services\UserService::class, \App\Services\UserService::class);
-         $this->app->bind(\App\Services\ProjectService::class, \App\Services\ProjectService::class);
-     
- 
+        
     }
 
     /**
